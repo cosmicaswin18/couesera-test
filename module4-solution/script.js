1e7f1bb584dfd6bd2651bbbf60e538611c3171aa
COSMICASWIN <cosmicaswin@gmail.com>
2018-09-02 10:49:25 +0530

    module4-solution

---
+++ module4-solution/script.js
@@ -0,0 +1,18 @@
+(function (){
+     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"]; 
+     for (var i = 0; i < names.length; i++) {
+        var first=names[i].charAt(0).toUpperCase();
+  
+
+  if (first === 'J') {
+    byeSpeaker.speak(names[i]);
+  
+  } 
+  else {
+    helloSpeaker.speak(names[i]);
+  }
+} 
+})();
+
+
+
